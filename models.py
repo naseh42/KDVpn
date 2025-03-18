2b6c7f154b26b5c99e9e162f654239fb3fcde794
naseh42 <naseh42@gmail.com>
2025-03-18 18:29:48 +0330

    Update models.py

--- models.py
+++ models.py
@@ -1,21 +1,23 @@
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy import Column, String, Integer, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime, timedelta
+from .database import Base
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True)
     uuid = Column(String, unique=True, index=True)
-    expiration_date = Column(DateTime)
-    usage = Column(Integer)
+    traffic_limit = Column(Integer)  # حجم مصرف
+    usage_duration = Column(Integer)  # مدت زمان استفاده (روزها)
+    expiry_date = Column(DateTime)  # تاریخ انقضا که محاسبه می‌شود از مدت زمان استفاده
+    simultaneous_connections = Column(Integer)  # محدودیت اتصال همزمان
+    active = Column(Boolean, default=True)  # وضعیت کاربر (فعال یا غیرفعال)
+    
+    server_id = Column(Integer, ForeignKey('servers.id'))
+    server = relationship("Server", back_populates="users")
 
-class Server(Base):
-    __tablename__ = 'servers'
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    ip_address = Column(String)
-    port = Column(Integer)
-    status = Column(String)
+    def set_expiry_date(self):
+        if self.usage_duration:
+            self.expiry_date = datetime.utcnow() + timedelta(days=self.usage_duration)
