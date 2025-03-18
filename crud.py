bfdd67c4d93cb7e383146444f6d12cdcbda9e592
naseh42 <naseh42@gmail.com>
2025-03-18 14:22:13 +0330

    Update and rename crud.py to user.html

--- crud.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-from .models import User, Server
-
-def get_users(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(User).offset(skip).limit(limit).all()
-
-def get_servers(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(Server).offset(skip).limit(limit).all()
